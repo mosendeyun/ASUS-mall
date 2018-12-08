0b6694441003dd8e96bd3e65365ac4ddfc98e16f
wang-jian-jun <1739192944@qq.com>
2018-12-08 12:55:11 +0800

    放大镜完善

---
+++ app/static/js/w-ej.js
@@ -0,0 +1,17 @@
+var er = (function () {
+    var liAll = document.querySelectorAll('.w-er1 li');
+    var boxAll = document.querySelectorAll('.w-er2 div');
+
+    var $liAll = $('.w-er1 li');
+    var $boxAll = $('.w-er2 div');
+    return {
+        init() {
+            this.event();
+        },
+        event() {
+            $liAll.mouseenter(function () {
+                $boxAll.eq
+            })
+        },
+    }
+}())
--- app/static/js/w-fdj.js
+++ app/static/js/w-fdj.js
@@ -1,13 +1,22 @@
 var fdj = (function () {
+    var $boxw = document.querySelector('.header2-1');
+    var header = document.querySelector('.header');
+
+    // var liAll = document.querySelectorAll('.uu li');
+
+
     var $box = document.querySelector('.h-l');
     var $mark = document.querySelector('.mark');
     var $showbox = document.querySelector('.h-r');
     var img = $showbox.querySelector('img');
+
+    var img2 = document.querySelector('.h-l img');
     return {
         init() {
             this.event();
         },
         event() {
+            // console.log(liAll);
             $box.onmouseenter = function () {
                 $mark.style.display = "block";
                 $showbox.style.display = "block";
@@ -17,21 +26,31 @@ var fdj = (function () {
                 $showbox.style.display = "none";
             }
             $box.onmousemove = function (e) {
+
                 var e = e || window.event;
                 var x = e.clientX, y = e.clientY;
-                var left = x - $mark.offsetWidth / 2 - this.offsetLeft;
+                // console.log(x);
+                var left = x - $mark.offsetWidth / 2 - header.offsetLeft;
+                // console.log(x, this.offsetLeft);
                 var top = y - $mark.offsetHeight / 2 - this.offsetTop;
+                // console.log($boxw.offsetTop);
                 var lmax = this.offsetWidth - $mark.offsetWidth;
                 var tmax = this.offsetHeight - $mark.offsetHeight;
                 left = left > lmax ? lmax : (left < 0 ? 0 : left);
                 top = top > tmax ? tmax : (top < 0 ? 0 : top)
-                console.log(left, top);
+                // console.log(left, top);
                 $mark.style.left = left + 'px';
                 $mark.style.top = top + 'px';
-                img.style.left = -2 * x + 'px';
-                img.style.top = -2 * y + 'px';
+                img.style.left = -2 * left + 'px';
+                img.style.top = -2 * top + 'px';
 
             }
+            $('.uu li').click(function () {
+                console.log($(this).index());
+                console.log(img);
+                img.src = `../images/w${$(this).index() + 1}.jpg`;
+                img2.src = `../images/w${$(this).index() + 1}.jpg`;
+            })
         },
         showImg() {
 
