adf44644bf525e93fbee7fccef919ef81b0e5ddf
mosendeyun <1049052243@qq.com>
2018-12-09 18:34:10 +0800

    good

--- app/static/js/shop_car.js
+++ app/static/js/shop_car.js
@@ -4,50 +4,51 @@ var shopCar = (function () {
     return {
         init() {
             this.getData();
+            this.allcheckbox()
         },
         event() {
             var _this = this;
-            $('.cart_action').click(function(){
-                let pNode=$(this).parent().parent().parent()
+            $('.cart_action').click(function () {
+                let pNode = $(this).parent().parent().parent()
                 console.log(_this.data);
-                _this.data.splice(pNode.index,1);
+                _this.data.splice(pNode.index, 1);
                 pNode.remove();
-                localStorage.shopList=JSON.stringify(_this.data)                
-           })
-           $(".count").blur(function(){
-               let index=$(this).parent().parent().parent().parent().parent().index()
-               let data=_this.data[index];
-               console.log( data.count)
-               data.count=$(this).val()
-               localStorage.shopList=JSON.stringify(_this.data)
+                localStorage.shopList = JSON.stringify(_this.data)
+            })
+            $(".count").blur(function () {
+                let index = $(this).parent().parent().parent().parent().parent().index()
+                let data = _this.data[index];
+                console.log(data.count)
+                data.count = $(this).val()
+                localStorage.shopList = JSON.stringify(_this.data)
                 _this.insertData(_this.data)
-           })
-           $('.btn_decrease').click(function(){
-               var val=$(this).next().val()*1
-               --val
-               if(val<1){
-                $(this).next().val(1)
-               }else{
-                   $(this).next().val(val)
-               }
-               let index=$(this).parent().parent().parent().parent().parent().index()
-               let data=_this.data[index];
-               data.count=val
-               _this.insertData(_this.data)
-           })
-           $('.btn_increase').click(function(){
-            var val=$(this).prev().val()*1
-            ++val
-            if(val<1){
-             $(this).prev().val(1)
-            }else{
-                $(this).prev().val(val)
-            }
-            let index=$(this).parent().parent().parent().parent().parent().index()
-            let data=_this.data[index];
-            data.count=val
-            _this.insertData(_this.data)
-        })
+            })
+            $('.btn_decrease').click(function () {
+                var val = $(this).next().val() * 1
+                --val
+                if (val < 1) {
+                    $(this).next().val(1)
+                } else {
+                    $(this).next().val(val)
+                }
+                let index = $(this).parent().parent().parent().parent().parent().index()
+                let data = _this.data[index];
+                data.count = val
+                _this.insertData(_this.data)
+            })
+            $('.btn_increase').click(function () {
+                var val = $(this).prev().val() * 1
+                ++val
+                if (val < 1) {
+                    $(this).prev().val(1)
+                } else {
+                    $(this).prev().val(val)
+                }
+                let index = $(this).parent().parent().parent().parent().parent().index()
+                let data = _this.data[index];
+                data.count = val
+                _this.insertData(_this.data)
+            })
 
 
 
@@ -63,18 +64,19 @@ var shopCar = (function () {
 
         },
         insertData(data) {
-            cartList.innerHTML = ''; 
-            var sum=0,total=0;        
+            cartList.innerHTML = '';
+            var sum = 0, total = 0;
             data.forEach((item, index) => {
                 var $div = document.createElement('div');
                 $div.index = index;
-                $div.className='cart_item'
+                $div.className = 'cart_item'
                 $div.innerHTML = `               
                     <div class="cart_product last">
                         <ul class="clearfix">
                             <li class="cart_input">
-                                <div class="checkbox_box checkAll on">
-                                    <i class="iconfont icon-gou"></i>
+                                <div class="checkbox_box on">
+                                    <i class=""></i>
+                                    <input type="checkbox" class="s_sec" name="checkAll">
                                 </div>
                             </li>
                             <li class="cart_goods">
@@ -99,8 +101,8 @@ var shopCar = (function () {
                                 <p class="goods_num_wrap">还可购买11件</p>
                             </li>
                             <li class="cart_discount">￥0</li>
-                            <li class="cart_point">${data[index].price*data[index].count}</li>
-                            <li class="cart_subtotal">${data[index].price*data[index].count}</li>
+                            <li class="cart_point">${data[index].price * data[index].count}</li>
+                            <li class="cart_subtotal">${data[index].price * data[index].count}</li>
                             <li class="cart_action">
                                 <a class="btn_delete cart_remove" href="javascript:"><i class="iconfont icon-cuo"></i></a>
                             </li>
@@ -117,17 +119,48 @@ var shopCar = (function () {
                         </div>
                     </div>
                 `
-                cartList.appendChild($div)              
-                sum+=data[index].count;
-                total+=data[index].price*data[index].count
+                cartList.appendChild($div)
+                sum += data[index].count;
+                total += data[index].price * data[index].count
             })
             console.log(sum)
             $('.allNum').html(data.length)
             $('.chooseNum').html(sum)
             $('.total_price b').html(total)
             this.event()
+        },
+        allcheckbox() {
+            console.log( $("[name=checkAll]"))
+            $("#allcheck").click(function () {                
+                if ($(this).prop("checked")) {
+                    $("[name=checkAll]").prop("checked", true);
+                    $(this).prev().addClass('iconfont checkAll icon-gou')
+                    $("[name=checkAll]").prev().addClass('iconfont checkAll icon-gou')
+                } else {
+                    console.log($(this).prop("checked"))
+                    $("[name=checkAll]").prop("checked", false);
+                    $(this).prev().removeClass('iconfont checkAll icon-gou')
+                    $("[name=checkAll]").prev().removeClass('iconfont checkAll icon-gou')
+                }
+            });
+            $("[name=checkAll]").click(function () {
+                var bool = true;
+                $(this).prev().toggleClass('iconfont checkAll icon-gou')       
+                $("[name=checkAll]").each(function () {
+                    console.log(11)
+                    if (!$(this).prop("checked")) {
+                        bool = false;
+                    }
+                });
+                if (bool) {
+                    $("#allcheck").prop("checked", true);
+                } else {
+                    $("#allcheck").prop("checked", false);
+                }
+            })
         }
     }
 
 }())
 shopCar.init()
+
