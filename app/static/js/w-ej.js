b0314ae78e0dd2d221fe41c0537849c718aa0804
mosendeyun <1049052243@qq.com>
2018-12-12 17:55:27 +0800

    合并文件完成

--- app/static/js/w-ej.js
+++ app/static/js/w-ej.js
@@ -1,29 +1,49 @@
 var er = (function () {
-
-    var $liAll = $('.w-er1 li');
-    var $boxAll = $('.uu');
-    var $all = $('.w-er');
     return {
         init() {
-            this.event();
+            this.event()
+            var mySwiper = new Swiper('.swiper-container', {
+                direction: 'horizontal', // 垂直切换选项
+                loop: true, // 循环模式选项
+        
+                // 如果需要分页器
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+        
+                // 如果需要前进后退按钮
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+        
+                // 如果需要滚动条
+                scrollbar: {
+                    el: '.swiper-scrollbar',
+                },
+                autoplay: {
+                    delay: 2000,
+                    stopOnLastSlide: false,
+                    disableOnInteraction: true,
+                },
+            })
         },
         event() {
-
-            $liAll.mouseover(function () {
+            $('.w-er1 li').mouseover(function () {
                 $('.w-er2').show();
                 console.log($(this).index());
-                $boxAll.eq($(this).index()).show().siblings('div').hide();
-                $boxAll.mouseover(function () {
+                $('.uu').eq($(this).index()).show().siblings('div').hide();
+                $('.uu').mouseover(function () {
                     $(this).show();
                 })
-                // $all.mouseover(function(){
-                //     $boxAll.eq(_this).show();
-                // })
             })
-            $all.mouseout(function () {
-                $boxAll.hide();
+            $('.w-er1 li').mouseout(function () {
+                $('.uu').hide();
             })
-
-        },
+            $('.w-er').mouseout(function () {
+                $('.uu').hide();
+            })
+        }
     }
 }())
