3a8778140fef6e4917a8f15e5c6745d441dc831d
mosendeyun <1049052243@qq.com>
2018-12-14 10:23:35 +0800

    修改gulp

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -1,9 +1,21 @@
-const devApi='http://10.36.141.228:1810/ASUS-mall/server/php/';
+// //登录注册
+// const devApi='http://10.36.141.228:1810/ASUS-mall/server/php/';
+// const buidldApi='/server/php';
+// var api=devApi;
+// //购物车地址
+// var shopping='http://10.36.141.233:8888/gitup/ASUS-mall/server/php/'
+// const apiObj = {
+//     login: api + 'denglu.php',
+//     register: api + 'zhuce.php',
+//     shopLis:shopping+'json.php'
+// }
+
+//登录注册
+const devApi='http://10.36.141.233:8888/gitup/ASUS-mall/server/php/';
 const buidldApi='/server/php';
 var api=devApi;
-var shopping='http://10.36.141.233:8888/gitup/ASUS-mall/server/php/'
 const apiObj = {
     login: api + 'denglu.php',
     register: api + 'zhuce.php',
-    shopLis:shopping+'json.php'
+    shopLis:api+'json.php'
 }
--- gulpfile.js
+++ gulpfile.js
@@ -11,57 +11,79 @@ var revCollector = require('gulp-rev-collector');//修改路径版本号
 var run = require('run-sequence');//异步执行
 var miniimg = require('gulp-imagemin');//图片压缩
 var less = require('gulp-less');//
+var rename = require('gulp-rename');
 
 
 //压缩js
-gulp.task('minijs',function(){
+gulp.task('minijs', function () {
     gulp.src('app/static/**/*.js')
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/static/'))
-    .pipe(connect.reload())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            // 添加后缀
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/static/'))
+        .pipe(connect.reload())
 })
 //压缩css
-gulp.task('minicss',function(){
+gulp.task('minicss', function () {
     gulp.src('app/static/**/*.css')
-    .pipe(minicss())
-    .pipe(gulp.dest('dist/static/'))
-    .pipe(connect.reload())
+        .pipe(minicss())
+        .pipe(rename({
+            // 添加后缀
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/static/'))
+        .pipe(connect.reload())
 })
 
 gulp.task('less', function () {
-      gulp.src('app/static/**/*.css')
-      .pipe(less())
-      .pipe(gulp.dest('dist/static'));
-  });
+    gulp.src('app/static/**/*.css')
+        .pipe(less())
+        .pipe(gulp.dest('dist/static'));
+});
 
 //压缩HTML
-gulp.task('minihtml',function(){
+gulp.task('minihtml', function () {
     gulp.src('app/*.html')
-    .pipe(minihtml())
-    .pipe(gulp.dest('dist'))
-    .pipe(connect.reload())//实时刷新
+        .pipe(minihtml())
+        .on('error', function (err) {
+            console.log(err.message);
+            this.emit('end')
+        })
+        .pipe(rename({
+            // 添加后缀
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload())//实时刷新
 })
 //图片压缩
-gulp.task('miniimg',function(){
+gulp.task('miniimg', function () {
     gulp.src('app/static/images/*')
-    .pipe(gulp.dest('dist/static/images'))
-    .pipe(connect.reload())
+        .pipe(miniimg())
+        .pipe(rename({
+            // 添加后缀
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/static/images'))
+        .pipe(connect.reload())
 })
 
 
 //监听实时更新
-gulp.task('watch',function(){
-    gulp.watch('dist/static/js/*.js',['minijs'])
-    gulp.watch('dist/static/**/*.css',['minicss'])
-    gulp.watch('dist/static/images/*',['miniimg'])
-    gulp.watch('dist/*.html',['minihtml'])
+gulp.task('watch', function () {
+    gulp.watch('dist/static/js/*.js', ['minijs'])
+    gulp.watch('dist/static/**/*.css', ['minicss'])
+    gulp.watch('dist/static/images/*', ['miniimg'])
+    gulp.watch('dist/*.html', ['minihtml'])
 })
 
 //打开服务器
-gulp.task('connect',function(){
+gulp.task('connect', function () {
     connect.server({
         root: 'dist',//服务器默认文件夹
         port: '7777',//端口号
@@ -71,21 +93,25 @@ gulp.task('connect',function(){
 
 
 //删除文件
-  gulp.task('clean', () => {
-    del(['dist', 'rev'])
+gulp.task('clean', () => {
+    del(['dist', 'rev']).then(_ => {
+        console.log('删除完毕')
+    })
 })
 
 //所有文件实时更新
-gulp.task('default', function() {
-   return run('clean',['minijs','miniimg','minicss'],
-    'minihtml',
-    'watch',
-    'connect',
+gulp.task('default', function () {
+    run(
+        'clean',
+        ['minijs', 'miniimg', 'minicss'],
+        'minihtml',
+        'watch',
+        'connect',
     )
-  })
-  gulp.task('dev',function(){
-     run(['clean'],['minijs','miniimg','minicss','minihtml'],['watch','connect'])
-  })
+})
+gulp.task('dev', function () {
+    run(['clean'], ['minijs', 'miniimg', 'minicss', 'minihtml'], ['watch', 'connect'])
+})
 
 
 
